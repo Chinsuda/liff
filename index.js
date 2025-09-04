e0d32aee026dcf43a1fc2b0e66fe5cf5ec55d108
Chinsuda <131727644+Chinsuda@users.noreply.github.com>
2025-09-04 23:37:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ async function main() {
   // Try a LIFF function
   async function getUserProfile() {
     const profile = await liff.getProfile();
-    pictureUrl.src = profile.pictureUrl;
-    userId.innerHTML = '<b>userId:</b> ' + profile.userId;
+    //pictureUrl.src = profile.pictureUrl;
+    //userId.innerHTML = '<b>userId:</b> ' + profile.userId;
     //statusMessage.innerHTML = '<b>statusMessage:</b> ' + profile.statusMessage;
     displayName.innerHTML =
       '<center><b>สวัสดีค่ะ คุณ</b> ' +
